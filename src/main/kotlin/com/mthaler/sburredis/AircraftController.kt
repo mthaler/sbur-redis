3cfc4671d089da10dd7736cdb35a43750c0af9ba
Michael Thaler <michael.thaler@dlr.de>
2021-06-13 12:02:07 +0200

    Added AircraftController.

---
+++ src/main/kotlin/com/mthaler/sburredis/AircraftController.kt
@@ -0,0 +1,15 @@
+package com.mthaler.sburredis
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.io.IOException
+
+@Controller
+class AircraftController(private val repository: AircraftRepository) {
+
+    @ResponseBody
+    @GetMapping("/aircraft")
+    @Throws(IOException::class)
+    fun getCoffees(): Iterable<Aircraft> = repository.findAll()
+}
